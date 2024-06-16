225b9327e1b7a601f5c6e08aee6ceccc7d9bcfa6
princessnaomii <103008328+princessnaomii@users.noreply.github.com>
2024-06-16 13:00:22 +0000

    Add files via upload

---
+++ abstract classes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace AbstractClassesDemo
+{
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5);
+            Rectangle rectangle = new Rectangle(4, 6);
+
+            Console.WriteLine($"Area of the circle: {circle.GetArea()}");
+            Console.WriteLine($"Area of the rectangle: {rectangle.GetArea()}");
+        }
+    }
+}
