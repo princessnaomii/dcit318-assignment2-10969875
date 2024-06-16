7474f6f6143331de8dbf13beb448e343149ba2d0
princessnaomii <103008328+princessnaomii@users.noreply.github.com>
2024-06-16 12:57:14 +0000

    Add files via upload

---
+++ inheritance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InheritanceDemo
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            genericAnimal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+        }
+    }
+}
